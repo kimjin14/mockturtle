91500d1ae65897162e8f5540a119e9310c79031b
Jin Hee <jinheek89@gmail.com>
2020-03-24 17:31:37 -0400

    Updating run_benchmark to be able to select between baseline run vs. carry mapping run.

--- run_benchmarks/run_benchmarks.cpp
+++ run_benchmarks/run_benchmarks.cpp
@@ -13,8 +13,10 @@
 #include <mockturtle/io/write_verilog.hpp>
 #include <mockturtle/algorithms/lut_mapping.hpp>
 #include <mockturtle/algorithms/carry_lut_mapping.hpp>
+//#include <mockturtle/algorithms/carry_chain_mapping.hpp>
 #include <mockturtle/algorithms/collapse_mapped.hpp>
 #include <mockturtle/networks/mig.hpp>
+#include <mockturtle/networks/xmg.hpp>
 #include <mockturtle/networks/klut.hpp>
 #include <mockturtle/algorithms/equivalence_checking.hpp>
 #include <mockturtle/algorithms/miter.hpp>
@@ -47,15 +49,16 @@ using namespace mockturtle;
 
 int main (int argc, char *argv[]){
 
-  if (argc < 2) return 0;
-
-  //std::cout << "benchmark,nMIG,dMIG,n6LUT,d6LUT\n";
+  bool runbaseline = false;
+  if (argc > 2 && argv[2] == std::string("baseline")) {
+    std::cout << "Baseline.\n";
+    runbaseline = true;
+  }
 
   mig_network mig;
   mig_network mig_original;
   lorina::diagnostic_engine diag;
   lorina::return_code result;
-
   
   /////////////////////////////
   // Read Verilog into a MIG network.
@@ -68,65 +71,75 @@ int main (int argc, char *argv[]){
   if (result != lorina::return_code::success) {
     std::cout << "Parsing Error.\n"; 
   }
-
   depth_view depth_mig { mig }; 
-  
-  ///////////////////////////// 
-  // Map MIG to LUT and carry
-  /////////////////////////////
-  mapping_view <mig_network, true> carry_mapped_mig { mig };
-  carry_lut_mapping <mapping_view<mig_network,true>,true> (carry_mapped_mig);  
-  const auto klut_carry_opt = collapse_mapped_network<klut_network>( carry_mapped_mig );
-  if (klut_carry_opt == std::nullopt) {
-    std::cout << "LUT and carry does not have mapping\n";
-    return 0;
-  }
-  auto const& klut_carry = *klut_carry_opt;
-  carry_depth_view depth_klut_carry { klut_carry }; 
-  //std::cout << "lut mapping finished\n";
-  write_dot(klut_carry, "carry.dot"); 
+  //xmg_network xmg;
+  //result = lorina::read_verilog(argv[1], verilog_reader(xmg) ,&diag);
+  //if (result != lorina::return_code::success) {
+  //  std::cout << "Parsing Error.\n"; 
+  //}
+  //depth_view depth_xmg { xmg };
 
+  //std::cout << "depth of xmg is " << depth_xmg.depth() << "\n";
+
+  
   /////////////////////////////
   // Map MIG to LUT only
   /////////////////////////////
-  mapping_view <mig_network, true> mapped_mig { mig };
-  lut_mapping <mapping_view<mig_network,true>,true> (mapped_mig); 
-  const auto klut_opt = collapse_mapped_network<klut_network>( mapped_mig );
-  if (klut_opt == std::nullopt) {
-    std::cout << "Does not have mapping\n";
-    return 0;
+  if (runbaseline) {
+    mapping_view <mig_network, true> mapped_mig { mig };
+    lut_mapping <mapping_view<mig_network,true>,true>(mapped_mig); 
+    const auto klut_opt = collapse_mapped_network<klut_network>( mapped_mig );
+    if (klut_opt == std::nullopt) {
+      std::cout << "Does not have mapping\n";
+      return 0;
+    }
+    auto const& klut = *klut_opt;
+    depth_view depth_klut ( klut ); 
+
+    //write_dot(klut, "output_lut.dot");
+    write_blif(klut, "blif/" + getFileName(argv[1]) + ".blif");  
+
+    std::cout << "Results for " << argv[1] << ",";
+    std::cout << mig.num_gates() << "," << depth_mig.depth();
+    std::cout << "," << klut.num_gates() << "," << depth_klut.depth();
+    std::cout << "\n";
   }
-  auto const& klut = *klut_opt;
-  depth_view depth_klut { klut }; 
-  write_dot(klut, "orig.dot"); 
-  
+  ///////////////////////////// 
+  // Map MIG to LUT and carry
   /////////////////////////////
-  // Check circuit equivalence.
-  /////////////////////////////
-  //const auto miter_circuit = miter<mig_network, mig_network, mig_network> (mig, mig_original);
-  //const auto miter_circuit = miter<klut_network, klut_network, mig_network> (klut_carry, mig_original);
-  /*const auto miter_circuit = miter<klut_network, klut_network, klut_network> (klut_carry, klut);
-  if ( miter_circuit == std::nullopt) std::cout << "Input/output numbers do not match.\n";
-  const auto result_of_equivalence = equivalence_checking ( *miter_circuit );
-  if ( !result_of_equivalence || ! *result_of_equivalence ) {
-    std::cout << "Networks are different.\n";
-    return 0;
-  }*/
+  else {
+    std::cout << "Creating mapping view\n";
+    mapping_view <mig_network, true> carry_mapped_mig { mig };
+    carry_lut_mapping <mapping_view<mig_network,true>,true> (carry_mapped_mig);  
+    const auto klut_carry_opt = collapse_mapped_network<klut_network>( carry_mapped_mig );
+    if (klut_carry_opt == std::nullopt) {
+      std::cout << "LUT and carry does not have mapping\n";
+      return 0;
+    }
+    auto const& klut_carry = *klut_carry_opt;
+
+    carry_depth_view depth_klut_carry { klut_carry }; 
+
+    //write_dot(klut_carry, "output_carry_lut.dot");
+    write_blif(klut_carry, "blif/" + getFileName(argv[1]) + "_carry.blif");  
+
+    std::cout << "Results for " << argv[1] << ",";
+    std::cout << mig.num_gates() << "," << depth_mig.depth();
+    std::cout << ",=" << klut_carry.num_gates() << "+" << klut_carry.num_carry() << "/2," << float(depth_klut_carry.depth()/LUT_DELAY);
+    std::cout << "\n";
+  }
 
   /////////////////////////////
   // Print mapping results.
   /////////////////////////////
-  std::cout << argv[1] << ",";
-  std::cout << mig.num_gates() << "," << depth_mig.depth() << ",";
-  std::cout << klut.num_gates() << "," << depth_klut.depth() << ",";
-  std::cout << klut_carry.num_gates() << "(" << klut_carry.num_carry() << ")," << float(depth_klut_carry.depth()/LUT_DELAY) << "\n";
+  //std::cout << "Results for " << argv[1] << ",";
+  //std::cout << mig.num_gates() << "," << depth_mig.depth();
+  //std::cout << "," << klut_carry.num_gates() << "(" << klut_carry.num_carry() << ")," << float(depth_klut_carry.depth()/LUT_DELAY);
+  //std::cout << "\n";
 
   /////////////////////////////
-  // Write to BLIF for VPR
+  // Write to Verilog for backup
   /////////////////////////////
-  write_blif(klut_carry, "blif/" + getFileName(argv[1]) + "_carry.blif", "output_carry.log");  
-  write_blif(klut, "blif/" + getFileName(argv[1]) + ".blif", "output.log");  
-
   //std::ofstream os( ("blif/" + getFileName(argv[1]) + "_carry.v").c_str(), std::ofstream::out );
   //write_verilog(mig, os);
   //std::ofstream os1( ("blif/" + getFileName(argv[1]) + ".v").c_str(), std::ofstream::out );
